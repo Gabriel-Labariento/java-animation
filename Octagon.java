9ff73d1da669d8a4b112fb76745c852000f0050e
Gabriel-Labariento <gabrielmatthew.labariento@gmail.com>
2025-02-14 09:25:23 +0800

    Starting.

--- Octagon.java
+++ Octagon.java
@@ -1,6 +1,6 @@
 import java.awt.Graphics2D;
 
-public class Triangle implements DrawingObject {
+public class Octagon implements DrawingObject {
     @Override
     public void draw(Graphics2D g2d){
         System.out.println("placeholder");
