fadf020c95452d9dfa581cfe68297d061854f348
Gabriel-Labariento <gabrielmatthew.labariento@gmail.com>
2025-02-16 11:07:52 +0800

    Finished octagon class.

--- Octagon.java
+++ Octagon.java
@@ -1,13 +1,37 @@
 import java.awt.*;
 import java.awt.geom.*;
 
+
 public class Octagon implements DrawingObject {
-    private double[] xPoints;
-    private double[] yPoints;
+    private Point2D.Double[] points;
     private Color color;
+    private boolean fill;
+
+    public Octagon(Point2D.Double[] points, Color color, boolean fill){
+        this.points = points;
+        this.color = color;
+        this.fill = fill;
+    }
 
     @Override
-    public void draw(Graphics2D g2d){
-        System.out.println("placeholder");
+    public void draw(Graphics2D g2d){        
+        if (points.length != 8) return;
+
+        Path2D.Double p = new Path2D.Double();
+
+        p.moveTo(points[0].getX(), points[0].getY());
+        for (int i = 1; i < 8; i++){
+            p.lineTo(points[i].getX(), points[i].getY());
+        }
+        p.closePath();
+
+        g2d.setColor(color);
+
+        if (fill) g2d.fill(p);
+        else g2d.draw(p);
+
+        
+        
     }
+
 }
--- Triangle.java
+++ Triangle.java
@@ -13,11 +13,11 @@ public class Triangle implements DrawingObject {
     }
 
     @Override
-    public void draw(Graphics2D g2d){
-        Path2D.Double p = new Path2D.Double();
-        
+    public void draw(Graphics2D g2d){        
         if (points.length != 3) return;
 
+        Path2D.Double p = new Path2D.Double();
+        
         p.moveTo(points[0].getX(), points[0].getY());
         for (int i = 1; i < 3; i++){
             p.lineTo(points[i].getX(), points[i].getY());
