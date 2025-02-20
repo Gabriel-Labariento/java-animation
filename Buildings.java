6af31a8cbd03f29abede7f7a50b2ac482dfb0b7a
Gabriel-Labariento <gabrielmatthew.labariento@gmail.com>
2025-02-20 20:17:48 +0800

    Finished buildings class.

--- Buildings.java
+++ Buildings.java
@@ -20,7 +20,9 @@ public class Buildings implements DrawingObject {
     }
 
     public void draw(Graphics2D g2d){
-
+        drawTertiaryBuildings(g2d);
+        drawSecondaryBuildings(g2d);
+        drawPrimaryBuildings(g2d);
     };
 
     public void adjustX(double distance){};
@@ -55,11 +57,92 @@ public class Buildings implements DrawingObject {
         g2d.fill(r2);
         g2d.setTransform(reset);
 
-        Rectangle2D.Double r3 = new Rectangle2D.Double(size*(x+65), size*(y+48.4), size*92.3, size*23.7);
-        g2d.rotate(Math.toRadians(90), r2.getX(), r2.getY());
-        g2d.fill(r2);
+        Rectangle2D.Double r3 = new Rectangle2D.Double(size*(x+95.3), size*(y+70.8), size*69.5, size*13.9);
+        g2d.rotate(Math.toRadians(90), r3.getX(), r3.getY());
+        g2d.fill(r3);
+        g2d.setTransform(reset);
+
+        Rectangle2D.Double r4 = new Rectangle2D.Double(size*(x+192.7), size*(y+44.5), size*95.9, size*31.3);
+        g2d.rotate(Math.toRadians(90), r4.getX(), r4.getY());
+        g2d.fill(r4);
+        g2d.setTransform(reset);
+
+        Rectangle2D.Double r5 = new Rectangle2D.Double(size*(x+204.5), size*(y+17.7), size*122.7, size*31.3);
+        g2d.rotate(Math.toRadians(90), r5.getX(), r5.getY());
+        g2d.fill(r5);
         g2d.setTransform(reset);
 
     }
 
+    private void drawPrimaryBuildings(Graphics2D g2d){
+        AffineTransform reset = g2d.getTransform();
+
+        g2d.setColor(c1);
+
+        Rectangle2D.Double r = new Rectangle2D.Double(size*(x+15.8), size*(y+70.8), size*69.5, size*15.8);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+37.6), size*(y+63.6), size*76.7, size*23.1);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+47.7), size*(y+102), size*38.4, size*13);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+77.9), size*(y+67.2), size*73.1, size*25.8);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+88.5), size*(y+78), size*62.3, size*13.2);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+97.2), size*(y+93.4), size*47, size*13.2);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+123.9), size*(y+47.1), size*93.2, size*28.6);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+141.2), size*(y+102), size*38.4, size*19.1);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+155.7), size*(y+63.6), size*76.7, size*23.1);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+173.2), size*(y+82.1), size*58.2, size*23.3);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+186.2), size*(y+32.8), size*107.4, size*17.3);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+204.5), size*(y+97.6), size*42.6, size*19.1);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+        r = new Rectangle2D.Double(size*(x+231), size*(y+58), size*82.3, size*31.3);
+        g2d.rotate(Math.toRadians(90), r.getX(), r.getY());
+        g2d.fill(r);
+        g2d.setTransform(reset);
+
+    }
 }
