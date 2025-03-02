8fdd0813d668bf8fe3c816ed4621aac25b21738a
Gabriel Labariento <gabrielmatthew.labariento@gmail.com>
2025-03-02 19:07:22 +0800

    Finished javadoc Scene0

--- Scene0.java
+++ Scene0.java
@@ -1,3 +1,25 @@
+/**
+        The Scene0 class creates a Scene0 object and has methods
+        for drawing it as it implements DrawingObject. This object
+        serves as the animation's title page. After the viewer
+        has exited this scene by pressing on the spacebar, the
+        animation will not loop back to it.
+   
+        @author Niles Tristan V. Cabrera ()
+        @author Gabriel Matthew P. Labariento (242425)
+        @version 03 March 2025
+
+        We have not discussed the Java language code in my program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
 import java.awt.*;
 import java.awt.geom.*;
 
@@ -6,6 +28,11 @@ public class Scene0 implements DrawingObject {
     private double width;
     private double height;
 
+    /**
+     * Initializes the object's field values to the provided arguments
+     * @param w the scene's width
+     * @param h the scene's height
+     */
     public Scene0(int w, int h){
         width = w;
         height = h;
