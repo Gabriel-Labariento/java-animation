9a6fbbe511c6610ad6523aea0293b5d63debdd9f
Gabriel Labariento <gabrielmatthew.labariento@gmail.com>
2025-03-02 18:53:53 +0800

    Finished javadoc Mouse object.

--- Mouse.java
+++ Mouse.java
@@ -1,3 +1,23 @@
+/**
+        The Mouse class creates a Mouse object and has methods
+        for drawing it as it implements DrawingObject. This object
+        appears in scene three as prey to the cat.
+   
+        @author Niles Tristan V. Cabrera ()
+        @author Gabriel Matthew P. Labariento (242425)
+        @version 03 March 2025
+
+        We have not discussed the Java language code in my program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
 import java.awt.*;
 import java.awt.geom.*;
 
@@ -8,6 +28,13 @@ public class Mouse implements DrawingObject {
     private double size;
     private Color color;
 
+    /**
+     * Initializes the object's field values to the arguments provided
+     * @param xPosition the object's x-coordinate
+     * @param yPosition the object's y-coordinate
+     * @param size the scaling applied to the object. If size = 1, the framing rectangle is 164.4px by 28.6px
+     * @param color
+     */
     public Mouse(double xPosition, double yPosition, double size, Color color){
         x = xPosition;
         y = yPosition;
@@ -27,12 +54,23 @@ public class Mouse implements DrawingObject {
     };
 
     @Override
-    public void setColor(Color color) {};
+    public void setColor(Color color) {
+        this.color = color;
+    };
     
+    /**
+     * Changes the object's position by some given distance along the x-axis
+     * @param distance the distance by which the object's position is changed
+     */
     public void adjustX(double distance){
         x += distance;
     };
 
+    /**
+     * Draws a mouse without the translation and scaling given to the constructor.
+     * Called inside the draw method
+     * @param g2d the object to manipulate geometric shapes
+     */
     private void drawMouseNoScaling(Graphics2D g2d){
         AffineTransform reset = g2d.getTransform();
         g2d.setColor(color);
