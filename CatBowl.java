5ef9f8933049931710ce3c99fbf7b2e1e194943b
Gabriel Labariento <gabrielmatthew.labariento@gmail.com>
2025-03-02 09:29:43 +0800

    Finished javadoc for CatBowl.

--- CatBowl.java
+++ CatBowl.java
@@ -1,3 +1,23 @@
+/**
+        The CatBowl class creates a CatBowl object and has methods
+        for drawing it as it implements DrawingObject. This object
+        has two instances in scene seven.
+   
+        @author Niles Tristan V. Cabrera ()
+        @author Gabriel Matthew P. Labariento (242425)
+        @version 03 March 2025
+
+        We have not discussed the Java language code in my program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
 import java.awt.*;
 import java.awt.geom.*;
 
@@ -5,6 +25,14 @@ public class CatBowl implements DrawingObject{
     private double x, y, size;
     private Color colorOne, colorTwo;
 
+    /**
+     * Initializes the CatBowl object's field to the provided arguments
+     * @param xPositon the x-coordinate of the object
+     * @param yPosition the y-coordinate of the object
+     * @param size the scaling applied to the object. If size = 1, the framing rectangle is 133.2px by 44.9px
+     * @param colorOne the outside color of the CatBowl
+     * @param colorTwo the inside color of the CatBowl
+     */
     public CatBowl(double xPositon, double yPosition, double size, Color colorOne, Color colorTwo){
         x = xPositon;
         y = yPosition;
