f61e1140c67d31db6fbbd3c146d9c84aa83fc9ec
Gabriel Labariento <gabrielmatthew.labariento@gmail.com>
2025-03-02 09:34:11 +0800

    Finished javadoc for Circle class.

--- Circle.java
+++ Circle.java
@@ -1,3 +1,25 @@
+/**
+        The Circle class creates a Circle object and has methods
+        for drawing it as it implements DrawingObject. This object
+        uses Ellipse2D.Double to create an ellipse of the same
+        height and width. This class is a fundamental part of other
+        objects in the animation. 
+   
+        @author Niles Tristan V. Cabrera ()
+        @author Gabriel Matthew P. Labariento (242425)
+        @version 03 March 2025
+
+        We have not discussed the Java language code in my program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
 import java.awt.*;
 import java.awt.geom.*;
 
@@ -8,6 +30,13 @@ public class Circle implements DrawingObject {
     private double size;
     private Color color;
 
+    /**
+     * Initializes the circle's field to the provided arguments.
+     * @param xPosition the x-coordinate of the framing rectangle's top-left corner
+     * @param yPosition the y-coordinate of the framing rectangle's top-left corner
+     * @param size the scaling applied to the object. If size = 1, the framing rectangle is 1px by 1px
+     * @param color the object's fill and outline color
+     */
     public Circle(double xPosition, double yPosition, double size, Color color){
         x = xPosition;
         y = yPosition;
@@ -23,9 +52,8 @@ public class Circle implements DrawingObject {
     }
 
     @Override
-    public void setColor(Color color) {};
+    public void setColor(Color color) {
+        this.color = color;
+    };
     
-    public void adjustX(double distance){
-        x += distance;
-    }
 }
