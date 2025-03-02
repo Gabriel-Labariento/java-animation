e0a927e58ca8a57f8032cb929ab37357413ae85e
Gabriel Labariento <gabrielmatthew.labariento@gmail.com>
2025-03-02 19:18:49 +0800

    Finished javadoc all scenes.

--- Scene5.java
+++ Scene5.java
@@ -1,3 +1,24 @@
+/**
+        The Scene5 class creates a Scene5 object and has methods
+        for drawing it as it implements DrawingObject. This object
+        uses Buildings, Cloud, Grass, and Circle classes to compose
+        the scene.
+   
+        @author Niles Tristan V. Cabrera ()
+        @author Gabriel Matthew P. Labariento (242425)
+        @version 03 March 2025
+
+        We have not discussed the Java language code in my program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
 import java.awt.*;
 import java.awt.geom.*;
 
@@ -5,6 +26,11 @@ public class Scene5 implements DrawingObject {
     private double width;
     private double height;
 
+    /**
+     * Initializes the object's field values to the provided arguments
+     * @param w the scene's width
+     * @param h the scene's height
+     */
     public Scene5(int w, int h){
         width = w;
         height = h;
