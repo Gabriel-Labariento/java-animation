4a75816ef5faf689b7f226f72a8b73fb04f0b37b
Gabriel Labariento <gabrielmatthew.labariento@gmail.com>
2025-03-02 17:40:06 +0800

    Finished javadoc FightCloud object.

--- fightCloud.java
+++ fightCloud.java
@@ -1,9 +1,36 @@
+/**
+        The FightCloud class creates a FightCloud object and has methods
+        for drawing it as it implements DrawingObject. This object
+        appears in scene four to illustrate the cat fight scene.
+   
+        @author Niles Tristan V. Cabrera ()
+        @author Gabriel Matthew P. Labariento (242425)
+        @version 03 March 2025
+
+        We have not discussed the Java language code in my program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
+
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 
 public class FightCloud implements DrawingObject{
     private double x, y, size;
 
+    /**
+     * Initializes the object's field values to the arguments provided.
+     * @param xPositon the object's x-coordinate
+     * @param yPosition the object's y-coordinte
+     * @param size the scaling applied to the object. If size = 1, the framing rectangle is 204.8px by 203.4px
+     */
     public FightCloud(double xPositon, double yPosition, double size){
         x = xPositon;
         y = yPosition;
@@ -22,10 +49,18 @@ public class FightCloud implements DrawingObject{
     @Override
     public void setColor(Color color) {}
 
+    /**
+     * Adjusts the object's position along the x-axis
+     * @param distance the distance to adjust the object's position by
+     */
     public void adjustX(double distance) {
         x += distance;
     }
 
+    /**
+     * Draws a cloud object without the translation and scaling given to the constructor. Called inside draw method.
+     * @param g2d object to manipulate geometric shapes
+     */
     private void drawFightCloudNoScaling(Graphics2D g2d){
         AffineTransform reset = g2d.getTransform();
 
