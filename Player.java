5657fa38dad00f50951de6b9c452693c0cb85020
legnspice <nilestristan@gmail.com>
2025-04-22 00:00:08 +0800

    see extended description
    -added collision detection, attack generation, gameloop management (multi-thread synchronization) for optimization, minor dev features (routing to available ports), abstract classes for player(to be implemented), attack, and enemy
    -TODO: Fix collision handling, detach attack movement from player movement(using keyinputs), and everything else

---
+++ Player.java
@@ -0,0 +1,36 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.*;
+
+public class Player extends Entity{
+
+    public Player(int cid, int x, int y){
+        this.clientId = cid;
+        this.identifier = 'A';
+        speed = 5;
+        height = 50;
+        width = 50;
+        health = 10;
+        damage = 5;
+        worldX = x;
+        worldY = y;
+        matchHitBoxBounds();
+    }
+ 
+    @Override
+    public void draw(Graphics2D g2d, int screenX, int screenY){
+        Rectangle2D.Double sprite = new Rectangle2D.Double(screenX, screenY, width, height);
+        g2d.setColor(Color.GREEN);
+        g2d.fill(sprite);
+    }
+
+    @Override
+    public void matchHitBoxBounds(){
+        // Bounds array is formatted as such: top, bottom, left, right; SIZES SUBJECT TO CHANGE PER ENTITY
+        hitBoxBounds = new int[4];
+        hitBoxBounds[0]= worldY;
+        hitBoxBounds[1] = worldY + height;
+        hitBoxBounds[2]= worldX;
+        hitBoxBounds[3] = worldX + width;
+    }
+}
